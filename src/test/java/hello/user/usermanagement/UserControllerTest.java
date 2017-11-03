7a9ae4e9fc5b3f3d2ede1bec1de6d6abba8f7ba0
SailajaVemuri <33132559+SailajaVemuri@users.noreply.github.com>
2017-11-03 10:58:16 +0530

    Controller unit test

--- src/test/java/hello/user/usermanagement/UserControllerTest.java
+++ src/test/java/hello/user/usermanagement/UserControllerTest.java
@@ -1,6 +1,5 @@
 package hello.user.usermanagement;
 
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import hello.user.usermanagement.controller.UserController;
 import hello.user.usermanagement.model.UserObject;
@@ -13,11 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
